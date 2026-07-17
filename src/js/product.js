53c11fd4164cd7b9a6419a3a0caf83256ee9fd0e
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-07-16 22:36:13 -0300

    att

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,42 @@
-import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+
+function getProductId() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("product");
+}
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
+
+async function addToCartHandler() {
+  const product = await dataSource.findProductById(getProductId());
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+async function init() {
+  const product = await dataSource.findProductById(getProductId());
+
+  document.querySelector("h2").textContent = product.Brand.Name;
+  document.querySelector("h3").textContent = product.Name;
+  document.getElementById("productPrice").textContent = `$${product.FinalPrice}`;
+  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
+  document.getElementById("productDesc").textContent = product.Description;
+
+  const image = document.getElementById("productImage");
+  image.src = product.Image;
+  image.alt = product.Name;
+
+  document
+    .getElementById("addToCart")
+    .setAttribute("data-id", product.Id);
+
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", addToCartHandler);
+}
+
+init();
